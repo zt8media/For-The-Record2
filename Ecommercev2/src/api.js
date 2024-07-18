7ca11915c7eb9abed323129b665400eeba0f46c4
zt8media <zt8media@gmail.com>
2024-07-18 17:37:56 -0400

    new

--- Ecommercev2/src/api.js
+++ Ecommercev2/src/api.js
@@ -1,23 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://for-the-record.onrender.com', // Update this to your remote server's URL
-  timeout: 30000
+  baseURL: 'http://172.31.19.35:5000', // Corrected URL format
+  timeout: 30000 // Timeout set for 30 seconds
 });
 
-export const fetchRecords = () => {
-  console.log('Fetching records...');
-  return api.get('/records')
-    .then(response => {
-      console.log('Records fetched:', response.data);
-      return response.data;
-    })
-    .catch(error => {
-      console.error('Error fetching records:', error);
-      throw error;
-    });
-};
-
+export const fetchRecords = () => api.get('/records');
 export const addRecord = (record) => api.post('/records', record);
 export const updateRecord = (id, record) => api.put(`/records/${id}`, record);
 export const deleteRecord = (id) => api.delete(`/records/${id}`);
